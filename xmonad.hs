615876b343e8750765de13f706d80a57ce12d9fc
Alexander Kojevnikov <alexander@kojevnikov.com>
2009-10-24 20:48:19 +1100

    doFullFloat shortcut

--- xmonad.hs
+++ xmonad.hs
@@ -1,10 +1,13 @@
 import XMonad
 import XMonad.Config.Gnome
 import XMonad.Actions.Submap
+import XMonad.Hooks.ManageHelpers
+import XMonad.Layout.NoBorders
 
 import Control.Arrow
 import Data.Bits
 import qualified Data.Map as M
+import Data.Monoid
 
 main :: IO ()
 main = do
@@ -12,11 +15,22 @@ main = do
          { terminal = "gnome-terminal"
          , focusFollowsMouse = False
          , borderWidth = 2
-         , keys = addPrefix (controlMask, xK_m) (keys gnomeConfig)
+         , keys = addPrefix (controlMask, xK_m) (newKeys)
+         , layoutHook = smartBorders $ layoutHook gnomeConfig
          }
+
+myKeys x =
+    [ ((modMask x, xK_f), fullFloatFocused)
+    ]
+
+newKeys x  =
+    M.union (keys gnomeConfig x) (M.fromList (myKeys x))
 
 addPrefix p ms conf =
     M.singleton p . submap $ M.mapKeys (first chopMod) (ms conf)
     where
     mod = modMask conf
     chopMod = (.&. complement mod)
+
+fullFloatFocused =
+    withFocused $ \f -> windows =<< appEndo `fmap` runQuery doFullFloat f
