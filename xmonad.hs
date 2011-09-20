c98bc23275255cf331993e48810401856ca8133a
Alexander Kojevnikov <alexander@kojevnikov.com>
2011-09-20 18:30:45 +0800

    xmonad.hs: Clean up

--- xmonad.hs
+++ xmonad.hs
@@ -17,29 +17,14 @@ import DBus
 import DBus.Connection
 import DBus.Message
 
--- This retry is really awkward, but sometimes DBus won't let us get our
--- name unless we retry a couple times.
-getWellKnownName :: Connection -> IO ()
-getWellKnownName dbus = tryGetName `catchDyn` (\ (DBus.Error _ _) ->
-                                                getWellKnownName dbus)
- where
-  tryGetName = do
-    namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
-    addArgs namereq [String "org.xmonad.Log", Word32 5]
-    sendWithReplyAndBlock dbus namereq 0
-    return ()
-
 main :: IO ()
-main =  withConnection Session $ \ dbus ->  do
-    putStrLn "Getting well-known name."
+main =  withConnection Session $ \ dbus -> do
     getWellKnownName dbus
-    putStrLn "Got name, starting XMonad."
     xmonad $ gnomeConfig
          { terminal = "gnome-terminal"
          , borderWidth = 2
          , keys = addPrefix (controlMask, xK_m) (newKeys)
          , layoutHook = smartBorders $ layoutHook gnomeConfig
---         , logHook = updatePointer (Relative 0.5 0.5)
          , logHook    = dynamicLogWithPP $ defaultPP {
                    ppOutput   = \ str -> do
                      let str'  = "<span font=\"Sans 10 Bold\">" ++ str ++ "</span>"
@@ -52,7 +37,7 @@ main =  withConnection Session $ \ dbus ->  do
                        (\ (DBus.Error _name _msg) ->
                          return 0)
                      return ()
-                 , ppTitle    = pangoColor "#FFFFFF" -- . shorten 60
+                 , ppTitle    = pangoColor "white"
                  , ppCurrent  = pangoColor "green" . wrap "[" "]"
                  , ppVisible  = pangoColor "yellow" . wrap "(" ")"
                  , ppHidden   = const "" --wrap " " " "
@@ -73,6 +58,33 @@ main =  withConnection Session $ \ dbus ->  do
              ]
          }
 
+newKeys x  =
+    M.union (keys gnomeConfig x) (M.fromList (myKeys x))
+  where
+    myKeys x =
+      [ ((modMask x, xK_f), fullFloatFocused)
+      ]
+
+addPrefix p ms conf =
+    M.singleton p . submap $ M.mapKeys (first chopMod) (ms conf)
+    where
+    mod = modMask conf
+    chopMod = (.&. complement mod)
+
+fullFloatFocused =
+    withFocused $ \f -> windows =<< appEndo `fmap` runQuery doFullFloat f
+
+-- xmonad-log-applet hook
+
+getWellKnownName :: Connection -> IO ()
+getWellKnownName dbus = tryGetName `catchDyn` (\(DBus.Error _ _) -> getWellKnownName dbus)
+  where
+    tryGetName = do
+        namereq <- newMethodCall serviceDBus pathDBus interfaceDBus "RequestName"
+        addArgs namereq [String "org.xmonad.Log", Word32 5]
+        sendWithReplyAndBlock dbus namereq 0
+        return ()
+
 pangoColor :: String -> String -> String
 pangoColor fg = wrap left right
  where
@@ -83,19 +95,3 @@ sanitize :: String -> String
 sanitize [] = []
 sanitize (x:rest) | fromEnum x > 127 = "&#" ++ show (fromEnum x) ++ ";" ++ sanitize rest
                   | otherwise        = x : sanitize rest
-
-myKeys x =
-    [ ((modMask x, xK_f), fullFloatFocused)
-    ]
-
-newKeys x  =
-    M.union (keys gnomeConfig x) (M.fromList (myKeys x))
-
-addPrefix p ms conf =
-    M.singleton p . submap $ M.mapKeys (first chopMod) (ms conf)
-    where
-    mod = modMask conf
-    chopMod = (.&. complement mod)
-
-fullFloatFocused =
-    withFocused $ \f -> windows =<< appEndo `fmap` runQuery doFullFloat f
